8b9daeb39525fea7ae17439761f85028998a0e1d
Ravi Ojha <rbojha008@gmail.com>
2017-03-22 02:36:29 +0530

    Using createStore from Redux and fist use of redux

--- app/index.js
+++ app/index.js
@@ -3,9 +3,9 @@ import { createStore } from "redux";
 const counter = (state = 0, action) => {
 	switch(action.type) {
 		case "INCREMENT":
-			return start + 1;
+			return state + 1;
 		case "DECREMENT":
-			return start - 1;
+			return state - 1;
 		default:
 			return 0;
 	}
@@ -13,3 +13,16 @@ const counter = (state = 0, action) => {
 
 const store = createStore(counter);
 
+const render = () => {
+	window.document.body.innerText = store.getState();	
+};
+
+store.subscribe(() => {
+	render()
+});
+
+window.document.addEventListener("click", () => {
+	store.dispatch({type: "INCREMENT"});
+});
+
+render();
