c233155beea9e9693309cc2654641b704fcdde2e
Ravi Ojha <rbojha008@gmail.com>
2017-03-22 03:04:08 +0530

    Finishing up without React

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
 
 const counter = (state = 0, action) => {
 	switch(action.type) {
@@ -7,8 +7,31 @@ const counter = (state = 0, action) => {
 		case "DECREMENT":
 			return state - 1;
 		default:
-			return 0;
+			return state;
 	}
+}
+
+var createStore = (reducer) => {
+	var state;
+	let listeners = [];
+
+	const getState = () => state;
+
+	const dispatch = (action) => {
+		state = reducer(state, action);
+		listeners.forEach(listener => listener());
+	}
+
+	const subscribe = (listener) => {
+		listeners.push(listener);
+		return () => {
+			listeners = listeners.filter(l => l != listener);
+		}
+	}
+
+	dispatch({});
+
+	return {getState, dispatch, subscribe};
 }
 
 const store = createStore(counter);
@@ -17,9 +40,7 @@ const render = () => {
 	window.document.body.innerText = store.getState();	
 };
 
-store.subscribe(() => {
-	render()
-});
+store.subscribe(render);
 
 window.document.addEventListener("click", () => {
 	store.dispatch({type: "INCREMENT"});
