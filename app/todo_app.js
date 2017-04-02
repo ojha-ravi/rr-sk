4b5080ca63562ef64cb8b4f9b5a1ed398539bd76
Ravi Ojha <rbojha008@gmail.com>
2017-04-02 22:59:26 +0530

    Some more refactoring

--- app/todo_app.js
+++ app/todo_app.js
@@ -1,60 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import * as Redux from "redux";
-
-const todo = (state, action) => {
-	switch(action.type) {
-		case "ADD_TODO":
-			return {
-				id: action.id,
-				text: action.text,
-				completed: false
-			};
-		case "TOGGLE_TODO":
-			if (state.id === action.id) {
-				// return Object.assign({}, state, {
-				// 	completed: !state.completed
-				// });
-				return {
-					...state,
-					completed: !state.completed
-				}
-			}
-			return state;
-		default:
-			return state;
-	}
-};
-
-const todos = (state = [], action) => {
-	switch(action.type) {
-		case "ADD_TODO":
-			return [
-				...state,
-				todo(undefined, action)
-			];
-		case "TOGGLE_TODO":
-			return state.map(t => todo(t, action));
-		default:
-			return state;
-	}
-};
-
-const visibilityFilter = (state = "SHOW_ALL", action) => {
-	switch(action.type) {
-		case "SET_VISIBILITY_FILTER":
-			return action.filter;
-		default:
-		return state;
-	}
-};
-
-const todoApp = Redux.combineReducers({
-	todos,
-	visibilityFilter
-});
-
-const store = Redux.createStore(todoApp);
+import { store } from "./todo_reducer"
 
 const getVisibleTodods = (todos, filter) => {
 	switch(filter) {
